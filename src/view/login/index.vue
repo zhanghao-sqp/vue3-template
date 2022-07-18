6bbe89662896f5613053782a94b8f1c2466b2e13
张浩 <blue1guys@gmail.com>
2022-07-18 10:32:37 +0800

    canvas

--- src/view/login/index.vue
+++ src/view/login/index.vue
@@ -1,12 +1,40 @@
 <template>
   <h2>login</h2>
-  <router-link to="/">去首页</router-link>
+  <router-link to="/">去首页</router-link><br>
+  <canvas id="canvas" width="600" height="500"></canvas>
 </template>
 
 <script lang="ts" setup>
-import {  } from 'vue'
+import { onMounted } from 'vue'
+onMounted(() => {
+  const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement
+  const ctx: CanvasRenderingContext2D = canvas.getContext('2d') as CanvasRenderingContext2D
+  // 画线
+  ctx.beginPath()
+  ctx.moveTo(0, 0)
+  ctx.lineTo(400, 300)
+  ctx.strokeStyle = 'red'
+  ctx.lineWidth = 3
+  ctx.stroke()
+  ctx.closePath()
+
+  ctx.beginPath()
+  ctx.moveTo(400, 0)
+  ctx.lineTo(0, 300)
+  ctx.strokeStyle = 'orange'
+  ctx.lineWidth = 3
+  ctx.stroke()
+  ctx.closePath()
+
+  ctx.closePath()
+
+})
 </script>
 
 <style scoped lang="scss">
-
+#canvas {
+  display: block;
+  margin: 0 auto;
+  background-color: #eee;
+}
 </style>
