e2e40fefed03984e54ce9aebcd265f473468d9cd
张浩 <1668471805@qq.com>
2022-08-14 23:02:52 +0800

    大文件切片上传

--- src/components/common/big-file-upload/index.vue
+++ src/components/common/big-file-upload/index.vue
@@ -1,12 +1,75 @@
 <template>
-  <input type="file">
+	<input class="file-upload-input" type="file" ref="fileUploadInputRef" @change="change" />
+  <el-button type="primary" :size="size" @click="uploadFile">上传</el-button>
+	<transition name="el-fade-in-linear">
+		<el-progress v-show="showProgress" :percentage="percent" />
+	</transition>
 </template>
 
 <script setup lang="ts">
-import {  } from 'vue'
+import { ref } from 'vue'
 
+withDefaults(
+	defineProps<{
+		uploadUrl?: string
+		size?: string
+	}>(),
+	{
+		uploadUrl: '',
+		size: 'default'
+	}
+)
+const fileUploadInputRef = ref<HTMLInputElement>()
+const showProgress = ref(false)
+const percent = ref(0)
+
+const uploadFile = () => {
+  fileUploadInputRef.value?.click()
+}
+const change = async (e: Event) => {
+	let chunkSize = 1024 * 1024 * 2 // 切片大小2M
+	const target = e.target as HTMLInputElement
+	if (target.files?.length) {
+		showProgress.value = true
+		const file = target.files[0]
+		const { name, size, type } = file
+		let start = 0,
+			index = 0
+		while (start < size) {
+			let blob = null
+			if (size - start > chunkSize) {
+				blob = file.slice(start, start + chunkSize)
+			} else {
+				blob = file.slice(start, size)
+			}
+			start += chunkSize
+			// let blobFile = new File([blob], `${index}.${type}`, { type })
+			let blobFile = new File([blob], name)
+			let formData = new FormData()
+			formData.append('file', blobFile)
+			formData.append('index', index.toString())
+			formData.append('name', name)
+			formData.append('size', size.toString())
+			formData.append('type', type)
+			index++
+			// 发送请求
+			// await axios.post('/upload', formData)
+			percent.value = Number(((start / size) * 100).toFixed(2))
+			if (percent.value >= 100) {
+				percent.value = 100
+				setTimeout(() => {
+					showProgress.value = false
+				}, 1000)
+			}
+		}
+	}
+}
 </script>
 
 <style scoped lang="scss">
-
+.file-upload-input {
+  width: 0;
+  height: 0;
+  visibility: hidden;
+}
 </style>
