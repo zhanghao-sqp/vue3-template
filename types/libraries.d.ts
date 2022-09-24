5c4fcf75a521fe56a2fd21adc411fa015a257a09
张浩 <1668471805@qq.com>
2022-09-24 19:15:16 +0800

    1

--- types/libraries.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'vue3-print-nb' {
-  export default function install(Vue: any): void
-}
-
-declare module 'vue3-json-excel' {
-  export const Vue3JsonExcel: DefineComponent<{}, {}, any>
-  export default Vue3JsonExcel
-}
